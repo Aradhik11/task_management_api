f97f702aa92cd3d7aeb74ef27c7f81babfb378ce
Dhikrullah Abdur-Rahmon <aradhik11@gmail.com>
2025-05-27 04:37:23 +0100

    nnew changes

--- src/config/database.ts
+++ src/config/database.ts
@@ -11,6 +11,12 @@ const sequelize = new Sequelize({
   password: process.env.DB_PASSWORD || '',
   dialect: 'postgres',
   logging: process.env.NODE_ENV === 'development' ? console.log : false,
+  dialectOptions: {
+    ssl: process.env.NODE_ENV === 'production' ? {
+      require: true,
+      rejectUnauthorized: false
+    } : false
+  },
   pool: {
     max: 10,
     min: 0,
